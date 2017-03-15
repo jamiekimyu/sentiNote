a45addd1f0019c50bb8dca1b1614a2da85d8fa4c
Alan Campbell <alancampbell4444@gmail.com>
2017-03-15 12:16:31 -0700

    journalskeleton

--- app/main.jsx
+++ app/main.jsx
@@ -5,9 +5,10 @@ import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
 import store from './store'
-import Jokes from './components/Jokes'
+
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
+import JournalInputContainer from './containers/JournalInputContainer'
 
 const ExampleApp = connect(
   ({ auth }) => ({ user: auth })
@@ -25,8 +26,9 @@ render (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
-        <Route path="/jokes" component={Jokes} />
+        <IndexRedirect to="/JournalInput"  component={JournalInputContainer} />
+        <Route path="/JournalInput" component={JournalInputContainer} />
+
       </Route>
     </Router>
   </Provider>,
--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 
 const rootReducer = combineReducers({
-  auth: require('./auth').default,  
+  auth: require('./auth').default, 
+  form: form 
 })
 
 export default rootReducer
