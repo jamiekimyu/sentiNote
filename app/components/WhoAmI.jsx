a6f3f8594cc051fa5c88b597073ba0f5d383d30d
Andrew Basore <basor1aj@cmich.edu>
2017-03-16 12:24:45 -0400

    Logout redirects to /home

--- app/components/WhoAmI.jsx
+++ app/components/WhoAmI.jsx
@@ -3,13 +3,14 @@ import {Link} from 'react-router';
 
 export const WhoAmI = ({ user, logout }) => (
   <div>
-  <Link to="/user">
-	  <div className="whoami">
-	    <span className="whoami-user-name">Hello, {user && user.name}!</span> <br />
+	  <Link to="/user">
+		  <div className="whoami">
+		    <span className="whoami-user-name">Hello, {user && user.name}!</span> <br />
 
-	  </div>
-  </Link>
-  <button className="logout btn btn-primary" onClick={logout}>Logout</button></div>
+		  </div>
+	  </Link>
+	  <Link to="/home"><button className="logout btn btn-primary" onClick={logout}>Logout</button></Link>
+  </div>
 )
 
 import {logout} from 'APP/app/reducers/auth'
