8a4ba028f08b440c75bcb46de61545ea41f68f5e
Jamie Yu <jme.kim.yu@gmail.com>
2017-03-16 17:19:28 -0400

    added colors and made graph smaller

--- app/components/Graphs/PieChart.jsx
+++ app/components/Graphs/PieChart.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { VictoryPie } from 'victory';
+import { VictoryPie, VictoryTheme } from 'victory';
  
 export default function PieChart ({sentimentObject, emotionObject}) {
-    //console.log('I GOT THE PROPS',sentimentObject, emotionObject); 
+//console.log('I GOT THE PROPS',sentimentObject, emotionObject); 
 
-//{anticipation: 3, joy: 3, positive: 3, trust: 3}
+//example emotionObject: {anticipation: 3, joy: 3, positive: 3, trust: 3}
 const dataRange = (emotionObject) => {
   let emotionArray = [];
   for(var emotion in emotionObject){
-    emotionArray.push({emotion: emotion, intensity: emotionObject[emotion]})  
+    if(emotion !== "positive" && emotion !== "negative"){
+      emotionArray.push({emotion: emotion, intensity: emotionObject[emotion]})  
+    }
   }
   return emotionArray
 };
@@ -19,6 +21,32 @@ const dataRange = (emotionObject) => {
         data={dataRange(emotionObject)}
         x="emotion"
         y="intensity"
+        
+        style={{
+          data: {
+            fill: (data) => {
+              if(data.x === "anger"){
+                return "red"
+              } else if(data.x === "anticipation"){
+                return "pink"
+              } else if(data.x === "disgust"){
+                return "gray"
+              } else if(data.x === "fear"){
+                return "black"
+              } else if(data.x === "joy"){
+                return "yellow"
+              } else if(data.x === "sadness"){
+                return "violet"
+              } else if(data.x === "surprise"){
+                return "purple"
+              } else if(data.x === "trust"){
+                return "orange"
+              } else {
+                return "white"
+              }
+            }
+          },
+        }}
       />
     );
 
