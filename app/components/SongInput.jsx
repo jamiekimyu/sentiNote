8ff72ffb665139c780d598a0bdee6754f5097dfd
Alan Campbell <alancampbell4444@gmail.com>
2017-03-16 19:22:57 -0700

    s

--- app/components/SongInput.jsx
+++ app/components/SongInput.jsx
@@ -65,7 +65,7 @@ class SongInput extends Component {
             <div>Content: {this.props.lyrics} </div>
           </div>
         </div>
-        <div>
+        <div className="flex-container noFlex">
           <h1>Graph</h1>
          <PieChart sentimentObject={sentimentObject} emotionObject={emotionObject}/>
         </div>
