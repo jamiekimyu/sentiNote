2d1dc9b130a669e0849aba60b44bdaca60f22084
Andrew Basore <basor1aj@cmich.edu>
2017-03-19 19:58:15 -0400

    profile is now centered with silver font

--- app/components/UserProfile.jsx
+++ app/components/UserProfile.jsx
@@ -11,7 +11,7 @@ export default function User (props) {
       <div className="content">
         <div className="userInfo">
           <h2>User Info</h2>
-          <p>Name: {user.name}</p>
+          <p className='userName'>{user.name}</p>
           <img className='profilePhoto' src={user.photoURL} />
           <p>Id: {user.id}</p>
         </div>
