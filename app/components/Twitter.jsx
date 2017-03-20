d0fe3a7d9e157d54dfd08377444399c18ef5dc59
Kim Winston-Jackson <klwjack@gmail.com>
2017-03-20 08:19:08 -0500

    search by term and topic almost completely working

--- app/components/Twitter.jsx
+++ app/components/Twitter.jsx
@@ -3,13 +3,19 @@ import Header from './Header';
 import Footer from './Footer';
 import Sidebar from './Sidebar';
 import {connect} from 'react-redux';
-import {getTweets} from '../reducers/twitter';
+import {getTweets, getSearchTweets, getTopicTweets} from '../reducers/twitter';
 
 const mapDispatchToProps = dispatch => {
   return {
     getTweets(screenName){
       dispatch(getTweets(screenName));
-    }
+    },
+		getSearchTweets(term){
+			dispatch(getSearchTweets(term));
+		},
+		getTopicTweets(topic){
+			dispatch(getTopicTweets(topic));
+		}
   };
 };
 
@@ -17,7 +23,9 @@ export class Twitter extends React.Component{
 	constructor(props){
 		super(props);
 		const initialState = {
-			twitterHandle: ''
+			twitterHandle: '',
+			twitterSearchTerm: '',
+			twitterTopic: ''
 		};
 		this.state = initialState;
 	}
@@ -29,6 +37,16 @@ export class Twitter extends React.Component{
 	handleSubmit(event){
 		event.preventDefault();
 		console.log(this.props.getTweets(this.state.twitterHandle));
+	}
+
+	handleSearchSubmit(event){
+		event.preventDefault();
+		console.log(this.props.getSearchTweets(this.state.twitterSearchTerm));
+	}
+
+	handleTopicSubmit(event){
+		event.preventDefault();
+		console.log(this.props.getTopicTweets(this.state.twitterTopic));
 	}
 
 	render(){
@@ -52,7 +70,33 @@ export class Twitter extends React.Component{
 		          <button type="submit" className="btn btn-primary">submit</button>
 		        </div>
 					</form>
-				</div>
+
+					<form onSubmit={(event) => this.handleSearchSubmit(event)}>
+						<div className="form-group">
+		          <label htmlFor="name" className="col-sm-2 control-label">Twitter Search Term:</label>
+		          <div className="col-sm-10">
+		            <input onChange={(event) => this.handleChange(event)} value={this.state.twitterSearchTerm} name="twitterSearchTerm" type="text" className="form-control" />
+		          </div>
+		        </div>
+
+						<div className="col-sm-offset-2 col-sm-10">
+		          <button type="submit" className="btn btn-primary">submit</button>
+		        </div>
+					</form>
+
+				<form onSubmit={(event) => this.handleTopicSubmit(event)}>
+					<div className="form-group">
+						<label htmlFor="name" className="col-sm-2 control-label">Twitter Search Term:</label>
+						<div className="col-sm-10">
+							<input onChange={(event) => this.handleChange(event)} value={this.state.twitterTopic} name="twitterTopic" type="text" className="form-control" />
+						</div>
+					</div>
+
+					<div className="col-sm-offset-2 col-sm-10">
+						<button type="submit" className="btn btn-primary">submit</button>
+					</div>
+				</form>
+			</div>
 
 				<Footer />
 		  </div>
--- app/reducers/twitter.js
+++ app/reducers/twitter.js
@@ -2,19 +2,30 @@ import axios from 'axios';
 
 /* -----------------    ACTIONS     ------------------ */
 const GET_TWEETS = 'GET_TWEETS';
+const GET_SEARCH_TWEETS = 'GET_SEARCH_TWEETS';
+const GET_TOPIC_TWEETS = 'GET_TOPIC_TWEETS';
 /* ------------   ACTION CREATORS     ------------------ */
 export const getTweetsAction = (tweets) => ({type: GET_TWEETS, tweets});
-
+export const getSearchTweetsAction = (searchTweets) => ({type: GET_SEARCH_TWEETS, searchTweets});
+export const getTopicTweetsAction = (topicTweets) => ({type: GET_TOPIC_TWEETS, topicTweets});
 /* ------------       REDUCER     ------------------ */
 const initialState = {
-	tweets: []
-}
+	tweets: [],
+	searchTweets: [],
+	topicTweets: []
+};
 
 export const reducer = (state = initialState, action) => {
 	const newState = Object.assign({}, state);
 	switch (action.type){
 		case GET_TWEETS:
-			newState.tweets = action.tweets;
+			newState.tweets = action.tweets.slice();
+			break;
+		case GET_SEARCH_TWEETS:
+			newState.searchTweets = action.searchTweets.slice();
+			break;
+		case GET_TOPIC_TWEETS:
+			newState.topicTweets = action.topicTweets.slice();
 			break;
 		default:
 			return state;
@@ -28,6 +39,28 @@ export const getTweets = (screenName) => {
          .then(response => response.data)
          .then((tweetData) => {
            dispatch(getTweetsAction(tweetData));
+         })
+				 .catch(err => console.error(err));
+  };
+};
+
+export const getSearchTweets = (term) => {
+  return dispatch => {
+    axios.get(`/api/twitter/search/${term}`)
+         .then(response => response.data)
+         .then((searchTweetData) => {
+           dispatch(getSearchTweetsAction(searchTweetData));
+         })
+				 .catch(err => console.error(err));
+  };
+};
+
+export const getTopicTweets = (topic) => {
+  return dispatch => {
+    axios.get(`/api/twitter/track/${topic}`)
+         .then(response => response.data)
+         .then((topicTweetData) => {
+           dispatch(getTopicTweetsAction(topicTweetData));
          })
 				 .catch(err => console.error(err));
   };
