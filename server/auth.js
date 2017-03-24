2e8b66ec6440eb88e75eeb388a521c976b745afd
Kim Winston-Jackson <klwjack@gmail.com>
2017-03-24 10:59:14 -0500

    facebook and google login working

--- server/auth.js
+++ server/auth.js
@@ -1,11 +1,14 @@
-const app = require('APP'), {env} = app
-const debug = require('debug')(`${app.name}:auth`)
-const passport = require('passport')
+const app = require('APP'), {env} = app;
+const debug = require('debug')(`${app.name}:auth`);
+const passport = require('passport');
 
-const User = require('APP/db/models/user')
-const OAuth = require('APP/db/models/oauth')
-const auth = require('express').Router()
+const User = require('APP/db/models/user');
+const OAuth = require('APP/db/models/oauth');
+const auth = require('express').Router();
 
+
+const dotenv = require('dotenv');
+dotenv.load();
 
 /*************************
  * Auth strategies
@@ -39,12 +42,13 @@ OAuth.setupStrategy({
   provider: 'facebook',
   strategy: require('passport-facebook').Strategy,
   config: {
-    clientID: env.FACEBOOK_CLIENT_ID,
-    clientSecret: env.FACEBOOK_CLIENT_SECRET,
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     callbackURL: `${app.baseUrl}/api/auth/login/facebook`,
+		profileFields: ['displayName', 'email']
   },
   passport
-})
+});
 
 // Google needs the GOOGLE_CLIENT_SECRET AND GOOGLE_CLIENT_ID
 // environment variables.
@@ -52,12 +56,13 @@ OAuth.setupStrategy({
   provider: 'google',
   strategy: require('passport-google-oauth').OAuth2Strategy,
   config: {
-    clientID: env.GOOGLE_CLIENT_ID,
-    clientSecret: env.GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: `${app.baseUrl}/api/auth/login/google`,
+		scope: ['email']
   },
   passport
-})
+});
 
 // Github needs the GITHUB_CLIENT_ID AND GITHUB_CLIENT_SECRET
 // environment variables.
@@ -70,56 +75,56 @@ OAuth.setupStrategy({
     callbackURL: `${app.baseUrl}/api/auth/login/github`,
   },
   passport
-})
+});
 
 // Other passport configuration:
 // Passport review in the Week 6 Concept Review:
 // https://docs.google.com/document/d/1MHS7DzzXKZvR6MkL8VWdCxohFJHGgdms71XNLIET52Q/edit?usp=sharing
 passport.serializeUser((user, done) => {
-  done(null, user.id)
-})
+  done(null, user.id);
+});
 
 passport.deserializeUser(
   (id, done) => {
-    debug('will deserialize user.id=%d', id)
+    debug('will deserialize user.id=%d', id);
     User.findById(id)
       .then(user => {
-        if (!user) debug('deserialize retrieved null user for id=%d', id)
-        else debug('deserialize did ok user.id=%d', id)
-        done(null, user)
+        if (!user) debug('deserialize retrieved null user for id=%d', id);
+        else debug('deserialize did ok user.id=%d', id);
+        done(null, user);
       })
       .catch(err => {
-        debug('deserialize did fail err=%s', err)
-        done(err)
-      })
+        debug('deserialize did fail err=%s', err);
+        done(err);
+      });
   }
-)
+);
 
 // require.('passport-local').Strategy => a function we can use as a constructor, that takes in a callback
 passport.use(new (require('passport-local').Strategy) (
   (email, password, done) => {
-    debug('will authenticate user(email: "%s")', email)
+    debug('will authenticate user(email: "%s")', email);
     User.findOne({where: {email}})
       .then(user => {
         if (!user) {
-          debug('authenticate user(email: "%s") did fail: no such user', email)
-          return done(null, false, { message: 'Login incorrect' })
+          debug('authenticate user(email: "%s") did fail: no such user', email);
+          return done(null, false, { message: 'Login incorrect' });
         }
         return user.authenticate(password)
           .then(ok => {
             if (!ok) {
-              debug('authenticate user(email: "%s") did fail: bad password')
-              return done(null, false, { message: 'Login incorrect' })
+              debug('authenticate user(email: "%s") did fail: bad password');
+              return done(null, false, { message: 'Login incorrect' });
             }
-            debug('authenticate user(email: "%s") did ok: user.id=%d', email, user.id)
-            done(null, user)
-          })
+            debug('authenticate user(email: "%s") did ok: user.id=%d', email, user.id);
+            done(null, user);
+          });
       })
-      .catch(done)
+      .catch(done);
   }
-))
+));
 
-auth.get('/whoami', (req, res) => res.send(req.user))
+auth.get('/whoami', (req, res) => res.send(req.user));
 
 // POST requests for local login:
 auth.post('/login/local', passport.authenticate('local', { successRedirect: '/', }))
@@ -132,12 +137,11 @@ auth.get('/login/:strategy', (req, res, next) =>
     successRedirect: '/',
     // Specify other config here, such as "scope"
   })(req, res, next)
-)
+);
 
 auth.post('/logout', (req, res, next) => {
-  req.logout()
-  res.redirect('/api/auth/whoami')
-})
+  req.logout();
+  res.redirect('/api/auth/whoami');
+});
 
-module.exports = auth
-
+module.exports = auth;
