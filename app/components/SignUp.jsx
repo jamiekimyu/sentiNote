a1093361cdd45c0b19e0c06508130067c2b06827
Andrew Basore <basor1aj@cmich.edu>
2017-03-27 16:58:53 -0400

    Signup Page is styled

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -2,31 +2,32 @@ import React from 'react';
 
 export  function Signup ({signup}){
   return (
-    <div className="flex-container">
-      <div className="content">
-      <h4>Sign up for a new account</h4>
+    <div className="container">
+      <div className="row title">
+        <h1>Sign up for a new account</h1>
+      </div>
+      <div className='row'>
+        <form onSubmit={evt => {
+          evt.preventDefault()
+          signup(evt.target.username.value, evt.target.email.value, evt.target.password.value)}}>
+          <div className="form-group">
+            <label>Name:       </label>
+            <input name="username" placeholder="name"/>
+          </div>
+          <div className="form-group">
+            <label>Email:  </label>
+            <input name="email" placeholder="email" />
+          </div>
+          <div className="form-group">
+            <label>Password:  </label>
+            <input name="password" type="password" placeholder="password"/>
+          </div>
+          <button type="submit" value="Signup" className="btn btn-primary">Sign Up </button>
+        </form>
+      </div>
 
-      <form onSubmit={evt => {
-        evt.preventDefault()
-        signup(evt.target.username.value, evt.target.email.value, evt.target.password.value)
-      } }>
-        <div className="form-group">
-          <label>Name:  </label>
-          <input name="username" placeholder="name"/>
-        </div>
-        <div className="form-group">
-          <label>Email:  </label>
-          <input name="email" placeholder="email" />
-        </div>
-        <div className="form-group">
-          <label>Password:  </label>
-          <input name="password" type="password" placeholder="password"/>
-        </div>
-        <button type="submit" value="Signup" className="btn btn-primary">Sign Up </button>
-      </form>
     </div>
-  </div>
-    );
+  );
 }
 
 import {signup} from 'APP/app/reducers/signup'
